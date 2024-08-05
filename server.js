f462aa63efaf433992c168cd6da08db3113887ea
RISHI VERMA <rishiv1000@outlook.com>
2024-08-05 15:33:34 +0530

    DONE2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const User = require ('./models/pass_model.js');
 const em_model = require ('./models/em_model.js');
